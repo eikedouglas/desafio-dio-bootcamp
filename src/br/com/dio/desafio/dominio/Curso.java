d5c087f012989d77b12a5265bc35fc842e5ac7fc
Eike <eikedouglas@hotmail.com>
2022-04-14 17:06:15 -0300

    first commit

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,45 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private String cargaHoraria;
+
+    public int calcularCargaXP() {
+        return 1;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria='" + cargaHoraria + '\'' +
+                '}';
+    }
+}
