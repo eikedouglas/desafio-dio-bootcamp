c9f76b109f7a6ecea301a33a78a2519ccf4ceb30
Eike <eikedouglas@hotmail.com>
2022-04-14 18:59:14 -0300

    metodos finalizados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
 
@@ -12,8 +16,23 @@ public class Main {
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria java");
         mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.parse("2022-04-14"));
 
-        System.out.println(curso);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("GFT Quality Assurance#1");
+        bootcamp.setDescricao("Descricao GFT Quality Assurance#1");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Eike Pires");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Dev:" + dev.getNome());
+        System.out.println("Conteudos inscritos: " + dev.getConteudosInscritos());
+        dev.progredir();
+        System.out.println("Conteudos inscritos: " + dev.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: " + dev.getConteudosConcluidos());
+        System.out.println("Total XP: " + dev.calcularTotalXP());
+
     }
 }
